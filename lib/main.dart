ff96fea78725b77a5a30502c26bae0d81ccd65a8
Apichit Mungudom <apichit55894@gmail.com>
2023-06-21 16:38:33 +0700

    Iot_farm

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:farm_iot/screen/page4.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
