4add72f8b891c55218915647ce3b039e0061e6b7
Apichit Mungudom <apichit55894@gmail.com>
2023-07-03 15:11:00 +0700

    fixed_error

--- lib/service/database.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-class DataSnapshot {
-  DatabaseReference databaseRef = FirebaseDatabase.instance.ref();
-}
